28a13041a317611afd7ed79e65a5a6193e82e22d
S0UL-R3AP3R <rober116@hotmail.com>
2019-08-08 14:00:30 -0500

    modifying test script

--- test_endpoint.sh
+++ test_endpoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-
+#
+#
 test_endpoint='curl -sL \
     -w "%{http_code}\\n" \
     "http://104.197.78.52:8081/" \
     -o /dev/null \
     --connect-timeout 3 \
     --max-time 5'
-if [ $(test_endpoint) = "200" ] ; 
+
+if [ $(test_endpoint) == "200" ];
 then
-   echo "OK" ;
+   echo "OK";
 else
-   echo "KO" ;
-   exit 1
+   echo "KO";
+   exit 1;
 fi
